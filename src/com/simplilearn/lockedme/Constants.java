7b3c29e862c878f02ab4981fe2a2ef1445a67776
phuthi ngoako <phuthin@hotmail.com>
2023-03-26 20:13:45 +0100

    US230303 - Constants

---
+++ src/com/simplilearn/lockedme/Constants.java
@@ -0,0 +1,52 @@
+package com.simplilearn.lockedme;
+
+public class Constants {
+    public static final String[] ARRAY_MAIN_MENU = {
+            "\tR - Retrieve File List",
+            "\tF - File Handling (Add, Delete, Search)",
+            "\tQ - Quit Application"
+    };
+    public static final String[] ARRAY_FILE_HANDLING_MENU = {
+            "\tA - Add New File",
+            "\tD - Delete Existing File",
+            "\tS - Search for a File",
+            "\tM - Back to Main Menu"
+    };
+    public static final String[] ARRAY_WELCOME_TEXT = {
+            "\n********************************************",
+            "\n**************   Welcome To  ***************",
+            "\n**************  LockedMe.com  **************",
+            "\n********************************************",
+            "\n*** Developed by : Phuthi N *** Mar 2023 ***",
+            "\n********************************************"
+    };
+    public static final String A_ADD_FILE = "A";
+    public static final String ALREADY_EXISTS = " already exists";
+    public static final String CLOSING_APP = "\tClosing your application... \n\tThank you!";
+    public static final String CREATED = " created";
+    public static final String D_DELETE_FILE = "D";
+    public static final String DELETED = " deleted";
+    public static final String ENTER_FILE_NAME_TO_ADD = "\nEnter the file name to add:\t";
+    public static final String ENTER_FILE_NAME_TO_DELETE = "\nEnter the file name to delete:\t";
+    public static final String ENTER_FILE_NAME_TO_SEARCH = "\nEnter the file name to search:\t";
+    public static final String ENTER_MENU_CHOICE = "\nEnter your choice:\t";
+    public static final String F_FILE_HANDLING = "F";
+    public static final String FILE = "\tFile";
+    public static final String FILE_HANDLING_MENU_TEXT = "\n* LockedMe *   File Handling Menu  *********";
+    public static final String FOLDER = "\tFolder";
+    public static final String FOLDER_FILE_COUNT = "\n\tCount of files found in the folder - ";
+    public static final String FOLDER_HAS_NO_FILES = "\nFolder has no files in it,\n\tFolder path - ";
+    public static final String FORWARD_SLASH = "/";
+    public static final String FOUND = " found";
+    public static final String INVALID_CHOICE_ENTERED = "\tYou have entered an invalid choice!\n\tTry again!\n";
+    public static final String M_MAIN_MENU = "M";
+    public static final String MAIN_MENU_TEXT = "\n* LockedMe *****   Main Menu   *************";
+    public static final String NO_FILES_TO_SEARCH_FROM = "\nThere are no files to search from";
+    public static final String NOT_FOUND = " not found";
+    public static final String Q_QUIT_APPLICATION = "Q";
+    public static final String R_RETRIEVE_FILE_LIST = "R";
+    public static final String S_SEARCH_FILE = "S";
+    public static final String WORKING_DIRECTORY_PATH =
+            "C:/Users/phuth/git/simplilearn.java.fsd.phase1.virtualkey/DummyTestFiles/";
+
+}
